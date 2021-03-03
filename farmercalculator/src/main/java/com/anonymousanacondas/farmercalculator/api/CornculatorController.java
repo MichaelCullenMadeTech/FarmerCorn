32cb60d90a46eab1bf61c2b8552feda9559aeae7
Michael <firstcapes@gmail.com>
2021-03-03 12:53:16 +0000

    Service set up to calculate the bags of corn, and simple test.

--- farmercalculator/src/main/java/com/anonymousanacondas/farmercalculator/api/CornculatorController.java
+++ farmercalculator/src/main/java/com/anonymousanacondas/farmercalculator/api/CornculatorController.java
@@ -2,6 +2,7 @@ package com.anonymousanacondas.farmercalculator.api;
 
 import com.anonymousanacondas.farmercalculator.dto.CornBags;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/cornculate")
 @RestController
 public class CornculatorController {
+
+    private final CornculatorService cornculatorService;
     
     @PostMapping
     public ResponseEntity<Double> postCornBags(@RequestBody CornBags cornBagsDto) {
+        double pricePerTrip = 0.25;
+
+
         return ResponseEntity.ok(0.0);
     }
     
